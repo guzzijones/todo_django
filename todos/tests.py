f314a4d69132fe723067b15a32cb660803f0b93e
guzzi <guzzijones12@gmail.com>
2018-12-25 21:12:32 -0500

    first

---
+++ todos/tests.py
@@ -0,0 +1,14 @@
+from django.test import TestCase
+from .models import Todo
+
+# Create your tests here.
+class TodoModelTest(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        Todo.objects.create(title="first todo")
+        Todo.objects.create(description="first description")
+
+    def test_title_content(self):
+        todo = Todo.objects.get(id=1)
+        expected_object_name = f'{todo.title}'
+        self.assertEquals(expected_object_name,"first todo")
